6ece34b210375013f17e67f7c5a7b7fbf23d5ffb
Mardoniosc <Mardonio@live.com>
2020-04-13 17:27:48 -0300

    :v: Aplicação Funcional (CRUD) completo

--- src/index.js
+++ src/index.js
@@ -1,53 +1,68 @@
 const express = require('express')
+const { uuid } = require('uuidv4')
 
 const app = express()
 app.use(express.json())
 
+
+const projects = []
+
+/**
+ * Rotas
+ */
 app.get('/projects', (req, res) => {
-  const { title, owner } = req.query;
+  const { title } = req.query;
 
-  console.log(title)
-  console.log(owner)
+  const results = title 
+    ? projects.filter( project => project.title.includes(title))
+    : projects
 
-  return res.send([
-    'projeto 1',
-    'projeto 2',
-    'projeto 3',
-  ])
+  return res.json(results)
 })
 
 app.post('/projects', (req, res) => {
   const {title, owner} = req.body;
-  console.log(title)
-  console.log(owner)
 
-  return res.send([
-    'projeto 1',
-    'projeto 2',
-    'projeto 3',
-    'projeto 4',
-  ])
+  const project = { id: uuid(), title, owner }
+
+  projects.push(project)
+  return res.json(project)
 })
 
 app.put('/projects/:id', (req, res) => {
   const { id } = req.params
+  const {title, owner} = req.body;
 
-  console.log(id)
+  const projectIndex = projects.findIndex(project => project.id === id)
 
-  return res.send([
-    'projeto 5',
-    'projeto 2',
-    'projeto 3',
-    'projeto 4',
-  ])
+  if(projectIndex < 0){
+    return res.status(400).json({ error: 'Project not found.' })
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  }
+
+  projects[projectIndex] = project
+
+  return res.json(project)
 })
 
 app.delete('/projects/:id', (req, res) => {
-  return res.send([
-    'projeto 5',
-    'projeto 2',
-    'projeto 4',
-  ])
+
+  const { id } = req.params
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if(projectIndex < 0){
+    return res.status(400).json({ error: 'Project not found.' })
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return res.status(204).send()
 })
 
 app.listen(4200, () => {
