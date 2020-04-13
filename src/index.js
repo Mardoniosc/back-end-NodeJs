ddadd608a91b4cd574dc97cfd11f86f1e94345b3
Mardoniosc <Mardonio@live.com>
2020-04-13 16:39:35 -0300

    Metodos HTTP

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.send({ msg: 'ok' })
+app.get('/projects', (req, res) => {
+  return res.send([
+    'projeto 1',
+    'projeto 2',
+    'projeto 3',
+  ])
+})
+
+app.post('/projects', (req, res) => {
+  return res.send([
+    'projeto 1',
+    'projeto 2',
+    'projeto 3',
+    'projeto 4',
+  ])
+})
+
+app.put('/projects/:id', (req, res) => {
+  return res.send([
+    'projeto 5',
+    'projeto 2',
+    'projeto 3',
+    'projeto 4',
+  ])
+})
+
+app.delete('/projects/:id', (req, res) => {
+  return res.send([
+    'projeto 5',
+    'projeto 2',
+    'projeto 4',
+  ])
 })
 
 app.listen(4200, () => {
