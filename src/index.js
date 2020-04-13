a8615656006fd7cb326000ba33d25e4a7fdd8f69
Mardoniosc <Mardonio@live.com>
2020-04-13 16:30:01 -0300

    :v: Configurando o nodemon

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/projects', (req, res) => {
+app.get('/', (req, res) => {
   return res.send({ msg: 'ok' })
 })
 
-app.listen()
+app.listen(4200, () => {
+  console.log('🚀️ Back-end started')
+})
